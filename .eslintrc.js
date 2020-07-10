2b47567820b331c13507ed5b438fe2a83dbeb7d3
onichandame <zxinmyth@gmail.com>
2020-07-10 03:27:46 +0000

    removed hook

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+module.exports = {
+  "extends": [
+    "react-app",
+    "@faasjs/recommended",
+    "plugin:prettier/recommended",
+    "prettier/@typescript-eslint",
+  ],
+  "settings": {
+    "react": {
+      "version": "999.999.999"
+    }
+  },
+  ignorePatterns: ["*.js"]
+}
---
+++ README.md
@@ -0,0 +1,13 @@
+# NATS Template
+
+This template is an example nats.io service people can use to build backend services based on nats.io.
+
+# Author
+
+[onichandame](https://onichandame.com)
+
+# Usage
+
+```bash
+yarn && yarn dev
+```
